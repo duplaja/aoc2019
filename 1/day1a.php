0f2b2597cb17e8d4791e66866648d43ccbcf9025
Dan D <dan.dulaney07@gmail.com>
2019-12-01 17:42:47 -0600

    Comment Day 1 solutions

--- 1/day1a.php
+++ 1/day1a.php
@@ -1,16 +1,24 @@
 <?php
+/*************************
+ * Day 1, part A: AoC 2019
+ ************************/
 
+ //Get All Inputs as Array
 $lines = file('day1input.txt');
 
+
+//Tracks total fuel needed
 $total = 0;
 
 foreach ($lines as $line) {
     
+    //Strip out any whitespace and newlines
     $mass = str_replace(array("\n", "\r", " "), '', $line);
 
-
+    //Calculate fuel needed based on formula from problem
     $individual = floor($mass/3)-2;
 
+    //Add to the total fuel required
     $total+= $individual;
 
 }
--- 1/day1b.php
+++ 1/day1b.php
@@ -1,16 +1,24 @@
 <?php
 
+/*************************
+ * Day 1, part B: AoC 2019
+ ************************/
+
+//Get All Inputs as Array
 $lines = file('day1input.txt');
 
+//Tracks total fuel needed
 $total = 0;
 
 foreach ($lines as $line) {
     
+    //Strip out any whitespace and newlines
     $mass = str_replace(array("\n", "\r", " "), '', $line);
 
-
+    //Calculate fuel needed based on formula from problem
     $individual = floor($mass/3)-2;
 
+    //Calculates fuel on the added fuel, repeating until 0 or negative.
     while($individual >=0 ) {
 
         $total+= $individual;
